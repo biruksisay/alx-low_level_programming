ecefc14271c273c79e6aec1a8d21fa99ec033af6
biruksisay <biruksisay70@gmail.com>
2022-06-13 06:36:00 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-  * reset_to_98 - Updates the value it points to 98
-  * @n: pointer to n
-  *
-  * Return: void
-  */
+ * reset_to_98 - change the value of integer using pointer
+ * @n: pointer
+ * Return:void
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;

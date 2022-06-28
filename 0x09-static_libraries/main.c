3f713fe7eee6b19147fd1a7aadac83cda443707e
biruksisay <biruksisay70@gmail.com>
2022-06-28 07:13:54 -0700

    main.c

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0
+ */
+int main(void)
+{
+_puts("\"Attheendoftheday,mygoalwasto be thebest hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
